86977b52b1e2ba294f3089070dbedfcd625c5354
oubella2 <oubellameryama@gmail.com>
2026-07-08 14:18:04 +0000

    Add Docker support and employee application

---
+++ employees/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render, redirect
+from .models import Employee
+from .forms import EmployeeForm
+
+def employee_list(request):
+    employees = Employee.objects.all()
+    return render(request, "employees/list.html", {"employees": employees})
+
+def employee_create(request):
+    if request.method == "POST":
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("employee_list")
+    else:
+        form = EmployeeForm()
+
+    return render(request, "employees/form.html", {"form": form})
